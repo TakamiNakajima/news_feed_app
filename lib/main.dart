8110ed0fd2e01945aeea62fddc88070b702c54a2
TakamiNakajima <takami.nakajima@gmail.com>
2022-12-19 23:19:00 +0900

    Create DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_feed/models/db/database.dart';
 import 'package:news_feed/view/screens/home_screen.dart';
 import 'package:news_feed/view/screens/pages/head_line_page.dart';
 import 'package:news_feed/view/style/style.dart';
@@ -6,7 +7,10 @@ import 'package:news_feed/view_models/head_line_viewmodel.dart';
 import 'package:news_feed/view_models/news_list_viewmodel.dart';
 import 'package:provider/provider.dart';
 
+late MyDatabase myDatabase;
+
 void main() {
+  myDatabase = MyDatabase();
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (context) => NewsListViewModel()),
--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -3,7 +3,10 @@ import 'dart:convert';
 import 'package:news_feed/data/category_info.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_feed/data/search_type.dart';
+import 'package:news_feed/main.dart';
 import 'package:news_feed/models/model/news_model.dart';
+import 'package:news_feed/util/extentions.dart';
+import 'package:news_feed/util/extentions.dart';
 
 class NewsRepository {
 
@@ -32,11 +35,23 @@ class NewsRepository {
     if (response.statusCode == 200) {
       //通常処理：受け取ったhttpステータスコードが200のときに実行
       final responseBody = response.body;
-      results = News.fromJson(jsonDecode(responseBody)).articles;
+      //results = News.fromJson(jsonDecode(responseBody)).articles;
+      results = await insertAndReadFromDB(jsonDecode(responseBody)) ;
     } else {
       //例外処理：受け取ったhttpステータスコードが200以外のときに実行
       throw Exception('Failed to load News');
     }
     return results;
+  }
+
+  Future<List<Article>> insertAndReadFromDB(responseBody) async{
+    final dao = myDatabase.newsDao;
+    final articles = News.fromJson(responseBody).articles;
+
+    //Webから取得した記事リスト（Dartのモデルクラス：Article）をDBのテーブルクラス（Articles）に変換してDB登録・DBから取得
+    final articleRecords = await dao.insertAndReadNewsFromDB(articles.toArticleRecords(articles));
+
+    //DBから取得したデータをモデルクラスに再変換して返す
+    return articleRecords.toArticles(articleRecords);
   }
 }
